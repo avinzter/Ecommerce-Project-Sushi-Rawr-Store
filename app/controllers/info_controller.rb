8ddba0d6894d57b9beca84d912e88d3d60fdbf3e
Geniva B. Lacara <geniva.lacara@gmail.com>
2013-11-15 05:17:52 -0600

    Added an info controller with five actions and associated views.

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,23 @@
+class InfoController < ApplicationController
+
+	def index
+	end
+	#Loads the app/view/info/index.html.erb
+
+	def menu
+	end
+	#Loads the app/view/info/menu.html.erb
+
+	def about
+	end
+	#Loads the app/view/info/about.html.erb
+
+	def contact_us
+	end
+	#Loads the app/view/info/contact.html.erb
+
+	def reviews
+	end
+	#Loads the app/view/info/review.html.erb
+
+end
