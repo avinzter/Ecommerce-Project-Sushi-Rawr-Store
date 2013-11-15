a97bd6640a59d452e36cf8809eeaad1560f2d52f
Geniva B. Lacara <geniva.lacara@gmail.com>
2013-11-15 04:53:14 -0600

    First commit of default rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SushiRawrStore::Application.config.secret_token = 'b4dcb5b551d821f70cdca05b9059fce1fb6bc688ac46205735cfe4ec912f491fbb9ba9cd338ffa3a0e4eea109f51fe02cb0fc4c92f24632dd93bfc0b6009fee2'
